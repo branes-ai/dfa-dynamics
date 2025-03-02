bafd85b2d0639bf1f47261e451de078011b0aa50
Ravenwater <theo@stillwater-sc.com>
2025-03-01 19:16:27 -0500

    bug fix Matrix operator*

--- include/dfa/matrix.hpp
+++ include/dfa/matrix.hpp
@@ -6,6 +6,7 @@
 
 namespace sw {
     namespace dfa {
+
         template<typename Scalar>
         class Matrix {
         private:
@@ -64,20 +65,19 @@ namespace sw {
                 return data[row];
             }
 
-
             Matrix operator*(const Matrix& other) const {
                 if (cols() != other.rows()) {
                     throw std::invalid_argument("Matrices cannot be multiplied.");
                 }
 
-                Matrix result{ {} }; // Initialize an empty matrix
-                result.data.resize(rows());
+                Matrix result(rows(), other.cols(), 0); // Initialize an zero result matrix
+                //result.data.resize(rows());
                 for (size_t i = 0; i < rows(); ++i) {
-                    result.data[i].resize(other.cols());
+                   // result.data[i].resize(other.cols());
                     for (size_t j = 0; j < other.cols(); ++j) {
-                        double sum = 0;
+                        Scalar sum = 0;
                         for (size_t k = 0; k < cols(); ++k) {
-                            sum += data[i][k] * other.get(k, j);
+                            sum += data[i][k] * other[k][j];
                         }
                         result.data[i][j] = sum;
                     }
@@ -102,5 +102,18 @@ namespace sw {
             }
         };
  
+        template<typename _T>
+        std::ostream& operator<<(std::ostream& ostr, const Matrix<_T>& M) {
+            ostr << '{';
+            for (int i = 0; i < M.rows(); ++i) {
+                ostr << '{';
+                for (int j = 0; j < M.cols(); ++j) {
+                    ostr << M[i][j];
+                    if (j < M.cols() - 1) ostr << ", ";
+                }
+                if (i < M.cols() - 1) ostr << "},\n"; else ostr << "}\n";
+            }
+            return ostr << "}\n";
+        }
     }
 }
--- src/dfa/tests/linalg.cpp
+++ src/dfa/tests/linalg.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
+#include <dfa/dfa.hpp>
 
 int main() {
+    using namespace sw::dfa;
 
+    Matrix<int> A = { {1, 0}, {0, 1} };
+    Matrix<int> B(A);
+
+    Matrix<int> C = A * B;
+
+    std::cout << C << '\n';
 
     return EXIT_SUCCESS;
 }
