a2478695c704d430918d5bf3cc4a2c17f05c50bc
Ravenwater <theo@stillwater-sc.com>
2025-03-03 18:46:54 -0500

    Introducing a PassManager test

--- tools/cli/mlir_txt_importer.cpp
+++ tools/cli/mlir_txt_importer.cpp
@@ -4,6 +4,14 @@
 #include <memory>
 #include <filesystem>
 
+#if WIN32
+#pragma warning(disable : 4244 4267)
+#endif
+
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/raw_ostream.h"
+
 #include "mlir/IR/Verifier.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/BuiltinOps.h"
@@ -11,14 +19,12 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"  // For func::FuncDialect
 #include "mlir/Dialect/Tosa/IR/TosaOps.h"
 #include "mlir/Parser/Parser.h"
-
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/raw_ostream.h"
-
-
-// compilation
-// g++ -std=c++17 -I/path/to/mlir/include -L/path/to/mlir/lib -lMLIRIR -lMLIRDialect -lMLIRParser -o mlir_importer mlir_importer.cpp
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/Passes.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/IR/Region.h"
+#include "mlir/IR/Block.h"
 
 namespace mlir {
     OwningOpRef<ModuleOp> read_mlirbc(const std::string& filepath, MLIRContext* context) {
@@ -55,7 +61,17 @@ namespace mlir {
 
         return module;
     }
-}
+
+    struct PrintNodeNamesPass : public PassWrapper<PrintNodeNamesPass, OperationPass<ModuleOp>> {
+        void runOnOperation() override {
+            ModuleOp module = getOperation();
+            module.walk([](Operation* op) {
+                llvm::outs() << op->getName() << "\n";
+                });
+        }
+    };
+
+} // namespace
 
 
 int main(int argc, char* argv[]) {
@@ -89,6 +105,22 @@ int main(int argc, char* argv[]) {
     // Use the parsed module as needed.
     std::cout << "Successfully deserialized module:\n";
     module->print(llvm::outs());
+
+    std::cout << "\n\n\n\n";
+    mlir::PassManager pm(&context);
+
+    // Add the custom pass to print node names
+    pm.addPass(std::make_unique<mlir::PrintNodeNamesPass>());
+
+    // Create an empty module
+    mlir::OpBuilder builder(&context);
+    //mlir::ModuleOp module = mlir::ModuleOp::create(builder.getUnknownLoc());
+
+    // Run the pass manager on the module
+    if (failed(pm.run(*module))) {
+        llvm::errs() << "PassManager execution failed!\n";
+        return 1;
+    }
 
     return EXIT_SUCCESS;
 }
