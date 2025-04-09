f445b94bcd7331cd983dd0f43172afe11955bd66
Ravenwater <theo@stillwater-sc.com>
2025-04-09 15:55:37 -0400

    expanding tensor parser to deal with undefined batch dimensions

--- tools/dfg/domain_flow_complexity.cpp
+++ tools/dfg/domain_flow_complexity.cpp
@@ -29,6 +29,14 @@ int main() {
     dfg.addNode(DomainFlowNode(DomainFlowOperator::MATMUL, "test.Matmul").addOperand("tensor<16x4x4xi16>").addOperand("tensor<1x4x4xi16>").addResult("out", "tensor<1x4x4xi16>"));
     dfg.addNode(DomainFlowNode(DomainFlowOperator::MATMUL, "test.Matmul").addOperand("tensor<32x4x4xi8>").addOperand("tensor<1x4x4xi8>").addResult("out", "tensor<1x4x4xi8>"));
 
+    // TOSA compliant 4D tensor = (N, H, W, C)
+    dfg.addNode(DomainFlowNode(DomainFlowOperator::CONV2D, "test.Conv2D").addOperand("tensor<1x224x224x3xf32>").addOperand("tensor<32x3x3x3xf32>").addResult("out", "tensor<1x112x112x32xf32>"));
+    dfg.addNode(DomainFlowNode(DomainFlowOperator::CONV2D, "test.Conv2D").addOperand("tensor<1x224x224x3xf16>").addOperand("tensor<32x3x3x3xf16>").addResult("out", "tensor<1x112x112x32xf16>"));
+    dfg.addNode(DomainFlowNode(DomainFlowOperator::CONV2D, "test.Conv2D").addOperand("tensor<1x224x224x3xf8>").addOperand("tensor<32x3x3x3xf8>").addResult("out", "tensor<1x112x112x32xf8>"));
+    dfg.addNode(DomainFlowNode(DomainFlowOperator::CONV2D, "test.Conv2D").addOperand("tensor<1x224x224x3xsi32>").addOperand("tensor<32x3x3x3xsi32>").addResult("out", "tensor<1x112x112x32xsi32>"));
+    dfg.addNode(DomainFlowNode(DomainFlowOperator::CONV2D, "test.Conv2D").addOperand("tensor<1x224x224x3xsi16>").addOperand("tensor<32x3x3x3xsi16>").addResult("out", "tensor<1x112x112x32xsi16>"));
+    dfg.addNode(DomainFlowNode(DomainFlowOperator::CONV2D, "test.Conv2D").addOperand("tensor<1x224x224x3xsi8>").addOperand("tensor<32x3x3x3xsi8>").addResult("out", "tensor<1x112x112x32xsi8>"));
+
     reportOperatorStats(dfg);
     reportArithmeticComplexity(dfg);
     reportNumericalComplexity(dfg);
