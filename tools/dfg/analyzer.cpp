2be4ee5af83c643a86ecea9ef626a653766b181c
Ravenwater <theo@stillwater-sc.com>
2025-04-10 16:24:17 -0400

    WIP: restructuring DFG component

---
+++ tools/dfg/analyzer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+#include <dfa/dfa.hpp>
+#include <util/data_file.hpp>
+
+int main(int argc, char** argv) {
+    using namespace sw::dfa;
+
+    if (argc != 2) {
+	    std::cerr << "Usage: " << argv[0] << " <DFG file>\n";
+        return EXIT_SUCCESS; // exit with success for CI purposes
+    }
+
+    std::string dataFileName;
+    try {
+        dataFileName = generateDataFile(argv[1]);
+        std::cout << "Data: " << dataFileName << std::endl;
+    } catch (const std::runtime_error& e) {
+	    std::cerr << "Error: " << e.what() << std::endl;
+		return EXIT_SUCCESS; // exit with success for CI purposes
+    }
+
+    DomainFlowGraph dfg(dataFileName); // Deep Learning graph
+    dfg.graph.load(dataFileName);
+
+    // report on the operator statistics
+    reportOperatorStats(dfg);
+
+    reportArithmeticComplexity(dfg);
+    reportNumericalComplexity(dfg);
+
+    return EXIT_SUCCESS;
+}
